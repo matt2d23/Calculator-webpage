da2025a42cadf0a1f067da349e5c5ea1d81a634e
matt2d23 <127718477+matt2d23@users.noreply.github.com>
2024-07-13 23:15:13 -0500

    Added the most f'd up algorithm of all time, currently only addition works

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 'use strict'
 
-function add(num1, num2) {
-    return num1 + num2;
+// calculator functions
+function add(num) {
+    let result = 0;
+    return result = num.reduce((a,b) => a+b);
 }
+
+
+
 
 function subtract(num1, num2) {
     return num1 - num2;
@@ -16,25 +21,82 @@ function divide(num1, num2) {
     return num1 / num2;
 }
 
-let firstOperator;
-let secondOperator;
+let num;
 let operatorType;
 
-function operate(operatorType, firstOperator, secondOperator) {
+function operate(operatorType, ...num) {
     let result;
     switch (operatorType) {
         case ('+'):
-            result = add(firstOperator, secondOperator);
+            result = add(num);
             break;
         case ('-'):
-            result = subtract(firstOperator, secondOperator);
+            result = subtract(num);
             break;
-        case ('*'):
-            result = multiply(firstOperator, secondOperator);
+        case ('x'):
+            result = multiply(num);
             break;
         case ('/'):
-            result = divide(firstOperator, secondOperator);
+            result = divide(num);
             break;
     }
     return result;
 }
+
+// DOM manipulation
+
+const resultBox = document.querySelector(".results");
+const buttons = document.querySelectorAll(".button");
+let currentValue;
+let prevValue;
+let values = [];
+let operator;
+
+function updateNumbers() {
+    buttons.forEach((button) => {
+        button.addEventListener("click", (value) => {
+            button.firstElementChild.classList.contains('one') ? value = 1 :
+            button.firstElementChild.classList.contains('two') ? value = 2 :
+            button.firstElementChild.classList.contains('three') ? value = 3 :
+            button.firstElementChild.classList.contains('four') ? value = 4 :
+            button.firstElementChild.classList.contains('five') ? value = 5 :
+            button.firstElementChild.classList.contains('six') ? value = 6 :
+            button.firstElementChild.classList.contains('seven') ? value = 7 :
+            button.firstElementChild.classList.contains('eight') ? value = 8 :
+            button.firstElementChild.classList.contains('nine') ?  value = 9 :
+            button.firstElementChild.classList.contains('zero') ? value = 0 :
+            value = '';
+    
+            currentValue = resultBox.textContent += value;
+
+            if (button.firstElementChild.classList.contains('clear')) {
+                resultBox.textContent = '';
+                currentValue = null;
+            }
+
+            if (button.classList.contains('operator')) {
+                if (values.length < 2) {
+                    prevValue = currentValue;
+                    operator = button.firstElementChild.textContent
+                    values.push(prevValue);
+                    values.push(operator);
+                    resultBox.textContent = '';
+                }
+               
+                
+            }
+            
+            if (button.firstElementChild.classList.contains('equals')) {
+                values.push(currentValue);
+                if (values.find((element) => element === '+')) {
+                    let newValues = values.slice(0, (values.indexOf('+') + 2));
+                    resultBox.textContent = add(newValues.filter((item) => +item).reduce((acc,x) => acc.concat(+x), []));
+                }
+                values = [];
+            }
+            
+        })
+    })
+}
+
+updateNumbers();
